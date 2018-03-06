4c953613ac2b81a41e70e34940ea765c5769abb0
zhengjg <zhengjinguangzjg@163.com>
2018-03-06 16:53:13 +0800

    add config

---
+++ cn.xsdzq.platfrom/src/main/java/cn/xsdzq/platfrom/controller/HelloController.java
@@ -0,0 +1,15 @@
+package cn.xsdzq.platfrom.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+	@RequestMapping("say")
+	public void sayHello() {
+		System.out.println("say hello");
+	}
+
+}
