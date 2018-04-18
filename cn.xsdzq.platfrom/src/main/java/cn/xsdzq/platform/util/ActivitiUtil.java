88efd721479b0f6bf402b690fae642edc0662d20
fanjx <fanjingxi1988@163.com>
2018-04-18 17:06:05 +0800

    添加activiti依赖的jar包以及demo类

---
+++ cn.xsdzq.platfrom/src/main/java/cn/xsdzq/platform/util/ActivitiUtil.java
@@ -0,0 +1,12 @@
+package cn.xsdzq.platform.util;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+
+public class ActivitiUtil {
+	private ProcessEngine processEngine;
+	public static void main(String[] args) {
+		ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+	//	processEngine.
+	}
+}
