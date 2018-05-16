934b0f13b5f77e8c3269c9ac723867848564c635
fanjingxi <fanjingxi1988@163.com>
2018-05-16 11:27:12 +0800

    新建信息插入用户完善2018.5.16

--- cn.xsdzq.platfrom/src/main/java/cn/xsdzq/platform/dao/InfoRepositoryImpl.java
+++ cn.xsdzq.platfrom/src/main/java/cn/xsdzq/platform/dao/InfoRepositoryImpl.java
@@ -7,6 +7,10 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,7 +37,11 @@ public class InfoRepositoryImpl implements InfoRepository {
 		TypedQuery<InfoEntity> sqlQuery = em.createQuery("SELECT c FROM InfoEntity c WHERE c.categoryId = ? and c.created_by = ?",
 				InfoEntity.class);
 		sqlQuery.setParameter(1, id);
-		sqlQuery.setParameter(2, "lishan");
+		SecurityContext ctx = SecurityContextHolder.getContext();
+		Authentication auth = ctx.getAuthentication();
+		User user = (User) auth.getPrincipal();
+		String  name = user.getUsername();
+		sqlQuery.setParameter(2, name);
 		return sqlQuery.getResultList();
 	}
 
@@ -79,9 +87,10 @@ public class InfoRepositoryImpl implements InfoRepository {
 	@Override
 	public List<InfoEntity> searUncheckchInfos() {
 		// TODO Auto-generated method stub
-				TypedQuery<InfoEntity> sqlQuery = em.createQuery("SELECT c FROM InfoEntity c WHERE c.checked = ?",
+				TypedQuery<InfoEntity> sqlQuery = em.createQuery("SELECT c FROM InfoEntity c WHERE c.checked = ? and c.checked_result = ?",
 						InfoEntity.class);
 				sqlQuery.setParameter(1, "N");
+				sqlQuery.setParameter(2, "submit");
 				return sqlQuery.getResultList();
 	}
 	
