c2538fb567c54955d194c06462a7afece0d82e83
fanjingxi <523777168@qq.com>
2018-11-16 18:05:17 +0800

    界面优化修改1116

--- cn.xsdzq.platfrom/src/main/webapp/static/htmls/info/infolist.js
+++ cn.xsdzq.platfrom/src/main/webapp/static/htmls/info/infolist.js
@@ -11,7 +11,9 @@ function infoListController($scope, $http, $state, $stateParams, $gridService, h
             待审核: "submit",
             已发布: "approve"
         };
-    
+    //拖动列宽
+
+    //拖动列宽end
 	$scope.init = function() {
 		//审核状态 默认为查询 全部
 		$scope.approveResult = $scope.approveResultOption['全部'];
@@ -53,6 +55,41 @@ function infoListController($scope, $http, $state, $stateParams, $gridService, h
 			}
 		}, true);
 	};
+	
+	
+	
+	//p begin
+	//创建变量用来保存选中结果
+	    $scope.selected = [];
+	    var updateSelected = function (action, id) {
+	      if (action == 'add' && $scope.selected.indexOf(id) == -1) $scope.selected.push(id);
+	      if (action == 'remove' && $scope.selected.indexOf(id) != -1) $scope.selected.splice($scope.selected.indexOf(id), 1);
+	    };
+	    //更新某一列数据的选择
+	    $scope.updateSelection = function ($event, id) {
+	      var checkbox = $event.target;
+	      var action = (checkbox.checked ? 'add' : 'remove');
+	      updateSelected(action, id);
+	    };
+	    //全选操作
+	    $scope.selectAll = function ($event) {
+	      var checkbox = $event.target;
+	      var action = (checkbox.checked ? 'add' : 'remove');
+	      for (var i = 0; i < $scope.infoList.length; i++) {
+	        var contact = $scope.infoList[i];
+	        updateSelected(action, contact.id);
+	      }
+	    };
+	    $scope.isSelected = function (id) {
+	      return $scope.selected.indexOf(id) >= 0;
+	    };
+	    $scope.isSelectedAll = function () {
+	      return $scope.selected.length === $scope.infoList.length;
+	    };
+    
+    
+    
+    // end
 	$scope.getInfosByCategoryId = function(pageSize) {
 		var inforTitle1 = $scope.formData.title;	
 		if (angular.isEmpty($scope.formData.title) ) {
@@ -77,6 +114,10 @@ function infoListController($scope, $http, $state, $stateParams, $gridService, h
 		$gridService.queryTableDatas($scope, tableElement, params, settings, $http);
 	};
 	$scope.modifyInfo = function(index) {
+		if($scope.selected.length > 1){
+        	layerUtils.iMsg(-1, "请选择单条记录编辑");
+			return;
+        }
 		layerUtils.iConfirm("是否修该此文章？", function() {
 			var info = $scope.infoList[index];
 			console.log(info);
@@ -98,8 +139,13 @@ function infoListController($scope, $http, $state, $stateParams, $gridService, h
 	}
 	
 	$scope.addWeight = function(index){
+        if($scope.selected.length > 1){
+        	layerUtils.iMsg(-1, "请选择单条记录置顶");
+			return;
+        }
 		var url = httpUtils.url.addWeight;
 		var param = $scope.infoList[index];
+		 console.log(param);
 		param.weight=param.weight+1;
 		$http.post(url, param).success(function(data) {
 			if (data.resCode == 0) {
@@ -109,6 +155,10 @@ function infoListController($scope, $http, $state, $stateParams, $gridService, h
 	}
 	
 	$scope.deleteInfo = function(index) {
+		if($scope.selected.length > 1){
+        	layerUtils.iMsg(-1, "请选择单条记录删除");
+			return;
+        }
 		layerUtils.iConfirm("是否删除该文章？", function() {
 			var url = httpUtils.url.deleteInfo;
 			var param = $scope.infoList[index];
@@ -123,6 +173,10 @@ function infoListController($scope, $http, $state, $stateParams, $gridService, h
 		});
 	}
 	$scope.copyUrl = function(index) {
+		if($scope.selected.length > 1){
+        	layerUtils.iMsg(-1, "请选择单条记录复制");
+			return;
+        }
 		console.log($scope.infoList[index]);
 		var info=$scope.infoList[index];
 		var text=window.location.protocol+"//"+window.location.hostname+":"+window.location.port+"/front/#/detail/"+info.id;
@@ -137,5 +191,125 @@ function infoListController($scope, $http, $state, $stateParams, $gridService, h
     	   textarea.style.display='none';
 	    }
 		layerUtils.iMsg(-1, "复制成功");
+	}
+	//批量置顶
+	$scope.batchAddWeight = function(){
+        if($scope.selected.length != 1){
+        	layerUtils.iMsg(-1, "请选择单条记录置顶");
+			return;
+        }
+        for (var h = 0; h < $scope.selected.length; h++) {			
+        	var infoId = $scope.selected[h];	
+  	      for (var i = 0; i < $scope.infoList.length; i++) {
+		        var tempInfo = $scope.infoList[i];
+		        if(tempInfo.id == infoId){
+		        	var param = tempInfo;
+		        }
+		      }        	
+        }  
+        console.log(param);
+		var url = httpUtils.url.addWeight;
+		//var param = $scope.infoList[index];
+		param.weight=param.weight+1;
+		$http.post(url, param).success(function(data) {
+			if (data.resCode == 0) {
+				layerUtils.iMsg(-1, "置顶成功");
+			}
+		});
+	}
+	//批量复制
+	$scope.batchCopyUrl = function() {
+		if($scope.selected.length != 1){
+        	layerUtils.iMsg(-1, "请选择单条记录复制！");
+			return;
+        }
+        for (var h = 0; h < $scope.selected.length; h++) {			
+        	var infoId = $scope.selected[h];
+   	      for (var i = 0; i < $scope.infoList.length; i++) {
+		        var tempInfo = $scope.infoList[i];
+		        if(tempInfo.id == infoId){
+		        	if(tempInfo.checked_result != 'approve'){
+		        		layerUtils.iMsg(-1, "未发布状态不可复制！");
+		    			return;
+		        	}
+		        	var param = tempInfo;
+		        }
+		      } 
+        }
+        console.log(param);
+        
+		var text=window.location.protocol+"//"+window.location.hostname+":"+window.location.port+"/front/#/detail/"+infoId;
+		if (window.clipboardData) {//如果是IE浏览器
+	        window.clipboardData.setData('text', text);
+	    } else {//非IE浏览器
+    	   var textarea = document.createElement('textarea');
+    	   textarea.value = text;
+           document.body.appendChild(textarea);
+           textarea.select(); // 选择对象
+           document.execCommand("Copy"); // 执行浏览器复制命令
+    	   textarea.style.display='none';
+	    }
+		layerUtils.iMsg(-1, "复制成功");
+	}
+	//批量编辑
+	$scope.batchModifyInfo = function() {
+		if($scope.selected.length != 1){
+        	layerUtils.iMsg(-1, "请选择单条记录编辑！");
+			return;
+        }
+		for (var h = 0; h < $scope.selected.length; h++) {			
+        	var infoId = $scope.selected[h];	
+  	      for (var i = 0; i < $scope.infoList.length; i++) {
+		        var tempInfo = $scope.infoList[i];
+		        if(tempInfo.id == infoId){
+		        	if(tempInfo.checked_result != 'generate'){
+		        		layerUtils.iMsg(-1, "非待提交状态不可编辑！");
+		    			return;
+		        	}
+		        	var param = tempInfo;
+		        }
+		      }        	
+        } 
+		console.log(param);
+		layerUtils.iConfirm("是否修该此文章？", function() {
+			console.log(param);
+			$state.go("modifyinfo", {
+				info : param
+			});
+		}, function() {
+			console.log("取消");
+		})
+	}
+	//批量删除
+	$scope.batchDeleteInfo = function() {
+		if($scope.selected.length != 1){
+        	layerUtils.iMsg(-1, "请选择单条记录删除！");
+			return;
+        }
+		for (var h = 0; h < $scope.selected.length; h++) {			
+        	var infoId = $scope.selected[h];	
+  	      for (var i = 0; i < $scope.infoList.length; i++) {
+		        var tempInfo = $scope.infoList[i];
+		        if(tempInfo.id == infoId){
+		        	if(tempInfo.checked_result != 'generate'){
+		        		layerUtils.iMsg(-1, "非待提交状态不可删除！");
+		    			return;
+		        	}
+		        	var param = tempInfo;
+		        }
+		      }        	
+        } 
+	
+		layerUtils.iConfirm("是否删除该文章？", function() {
+			var url = httpUtils.url.deleteInfo;
+			$http.post(url, param).success(function(data) {
+				if (data.resCode == 0) {
+					layerUtils.iMsg(-1, "删除成功");
+					$scope.getInfosByCategoryId(10);
+				}
+			});
+		}, function() {
+			console.log("取消");
+		});
 	}
 }
