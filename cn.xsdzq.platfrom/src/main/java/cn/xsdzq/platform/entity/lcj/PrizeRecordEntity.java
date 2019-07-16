517fa24011e9626bc9bea7c1ba884203cfc99533
fanjingxi <523777168@qq.com>
2019-07-16 19:12:49 +0800

    更新实体类

--- cn.xsdzq.platfrom/src/main/java/cn/xsdzq/platform/entity/lcj/PrizeRecordEntity.java
+++ cn.xsdzq.platfrom/src/main/java/cn/xsdzq/platform/entity/lcj/PrizeRecordEntity.java
@@ -1,100 +1,106 @@
 package cn.xsdzq.platform.entity.lcj;
 
+import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 @Entity
 @Table(name = "lcj_prize_record")
 @EntityListeners(AuditingEntityListener.class)
-public class PrizeRecordEntity {
-	
+public class PrizeRecordEntity implements Serializable {
 	private static final long serialVersionUID = 1L;
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "prize_record_sequence")
-	@SequenceGenerator(name = "prize_record_sequence", sequenceName = "sequence_prize_record", allocationSize = 1)
-	@Column(name = "id")
-	private long id;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", unique = true, length = 20)
+	private Long id;
+
+	@Column(name = "type", nullable = false)
+	private boolean type; // type 为0 表示减少 reason 以0开头 0X， 为1表示增加 reason 以1开头 1X
+
+	@Column(name = "reason", nullable = false)
+	private String reason;
+
+	@Column(name = "num", nullable = false)
+	private Integer number = 0; // 增加或者减少的数量,默认为0
 	
-	@Column(name = "username")
-	private String username;
-	
-	@Column(name = "account")
-	private String account;//资金账号
-	
-	@Column(name = "client_id")
-	private String clientId;//客户号
-	
-	@Column(name = "prize_id")
-	private String prizeId;
-	
-	@Column(name = "prizeName")
-	private String prizeName;
-	
-	@Column(name = "createtime")
-	private Date createtime;
+	@Column(name = "data_flag", nullable = false)
+	private String dateFlag; // 每日的判断标准
 
-	
+	@Column(name = "record_time", nullable = false)
+	private Date recordTime;
 
-	public String getUsername() {
-		return username;
-	}
+	@ManyToOne(cascade = { CascadeType.MERGE, CascadeType.REFRESH }, fetch = FetchType.EAGER, optional = false)
+	@JoinColumn(name = "client_id", referencedColumnName = "client_id")
+	private LcjUserEntity userEntity;
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getAccount() {
-		return account;
-	}
-
-	public void setAccount(String account) {
-		this.account = account;
-	}
-
-	public String getPrizeName() {
-		return prizeName;
-	}
-
-	public void setPrizeName(String prizeName) {
-		this.prizeName = prizeName;
-	}
-
-	
-
-	public Date getCreatetime() {
-		return createtime;
-	}
-
-	public void setCreatetime(Date createtime) {
-		this.createtime = createtime;
-	}
-
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
-	@Override
-	public String toString() {
-		return "PrizeRecord [id=" + id + ", username=" + username + ", account=" + account + ", prizeName=" + prizeName
-				+ ", createtime=" + createtime + "]";
+	public boolean isType() {
+		return type;
 	}
 
-	
+	public void setType(boolean type) {
+		this.type = type;
+	}
 
-	
-	
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getDateFlag() {
+		return dateFlag;
+	}
+
+	public void setDateFlag(String dateFlag) {
+		this.dateFlag = dateFlag;
+	}
+
+	public Date getRecordTime() {
+		return recordTime;
+	}
+
+	public void setRecordTime(Date recordTime) {
+		this.recordTime = recordTime;
+	}
+
+	public LcjUserEntity getUserEntity() {
+		return userEntity;
+	}
+
+	public void setUserEntity(LcjUserEntity userEntity) {
+		this.userEntity = userEntity;
+	}
 
 }
