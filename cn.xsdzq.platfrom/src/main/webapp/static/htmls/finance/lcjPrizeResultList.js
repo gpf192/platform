b56c15aeaf46e1d18cd4bc071de7ee4508abb2ab
fanjingxi <523777168@qq.com>
2020-08-03 13:24:15 +0800

    各导出页面导出功能名称优化

--- cn.xsdzq.platfrom/src/main/webapp/static/htmls/finance/lcjPrizeResultList.js
+++ cn.xsdzq.platfrom/src/main/webapp/static/htmls/finance/lcjPrizeResultList.js
@@ -85,9 +85,26 @@ function lcjPrizeResultListController($scope, $http, $state, httpUtils, layerUti
 	
 
 	//导出为excel
+	
+	Date.prototype.Format = function (fmt) {  
+	    var o = {
+	        "M+": this.getMonth() + 1, //月份 
+	        "d+": this.getDate(), //日 
+	        "h+": this.getHours(), //小时 
+	        "m+": this.getMinutes(), //分 
+	        "s+": this.getSeconds(), //秒 
+	        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+	        "S": this.getMilliseconds() //毫秒 
+	    };
+	    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+	    for (var k in o)
+	    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+	    return fmt;
+	}
+	
 	$scope.exportToExcel=function(){ 
 		var excelArrs = getExcelData();
-		var myDate = new Date();
+		var myDate = new Date().Format("yyyyMMddhhmmss");
 		 alasql.promise('SELECT * INTO XLSX("中奖记录表-' + myDate+ '.xlsx",{headers:true}) FROM ?',[excelArrs])
 			.then(function (data) {
 			  if(data == 1){
